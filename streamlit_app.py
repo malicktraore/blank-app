4619997ca440073674853d52efb165511a101a87
malicktraore <137112119+malicktraore@users.noreply.github.com>
2024-12-20 10:42:31 +0000

    ok1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import pandas as pd
+
+
+
 
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+def load_excel_data(file_path):
+    df = pd.read_excel(file_path)
+    return df
